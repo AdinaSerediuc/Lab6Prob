105d0f8f1fd4b6d4e6c34f6ea86afd6cfecaeaf8
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-10-23 20:29:54 +0300

    Add project files.

---
+++ Lab6Prob/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab6Prob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
